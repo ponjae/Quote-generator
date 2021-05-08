008c65c2e96b8b33f95b65a0f6d1c2c7f173843a
ponjae <>
2021-05-08 17:11:07 +0200

    Added functionality to generate a new quote and tweet the quote

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+
+
 let apiQuotes = [];
 
 
 //Get a new random quote from the list
 function singleQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-    console.log(quote)
+    if (!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author;
+    }
+    if (quote.text.length > 95) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes from API
@@ -18,6 +35,16 @@ async function getQuotes() {
         console.log('Error: ', err)
     }
 }
+
+// Tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+// Event listerner
+newQuoteBtn.addEventListener('click', singleQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 // On load 
 getQuotes()
